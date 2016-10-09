4aae22a7e55c5654f8bbae0213f1167554eb184b
Mark Lopez <m@silvenga.com>
2016-10-09 08:46:34 -0500

    Added GlobalExtensions, refactored.

---
+++ src/Hangfire.MetaExtensions/GlobalExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Hangfire.MetaExtensions
+{
+    public static class GlobalExtensions
+    {
+        public static IGlobalConfiguration UseMetaExtensions(this IGlobalConfiguration configuration)
+        {
+            configuration.UseFilter(new MetaClientFilterAttribute());
+            return configuration;
+        }
+    }
+}
--- src/Hangfire.MetaExtensions/MetaClientExtensions.cs
+++ src/Hangfire.MetaExtensions/MetaClientExtensions.cs
@@ -2,13 +2,29 @@
 
 using Hangfire.Client;
 
+using JetBrains.Annotations;
+
 namespace Hangfire.MetaExtensions
 {
     public static class MetaClientExtensions
     {
-        public static IBackgroundJobClient UseMeta<T>(IBackgroundJobClient client, string key, T value)
+        public static IBackgroundJobClient AddOrUpdateMeta<T>([NotNull] this IBackgroundJobClient client, [NotNull] string key, [NotNull] T value)
         {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
             ThreadStorage<Action<CreatingContext>>.Add(context => context.SetJobParameter(key, value));
+
             return client;
         }
     }
